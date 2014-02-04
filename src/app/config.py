5f86c5c63eca122d9d94268608d8cc4ef2e40271
nbeck <nicbec@gmail.com>
2014-02-03 20:30:33 -0800

    took variables from __init__.py

---
+++ src/app/config.py
@@ -0,0 +1,6 @@
+#No idea what the end goal is for these variables yet, or what format is proper
+
+class _DefaultSettings(object):
+    USERNAME = 'world'
+    SECRET_KEY = 'development key'
+    DEBUG = True
