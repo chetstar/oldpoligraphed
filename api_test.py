af081b49d1190c2ea1770e5857b8a368f24485ca
Cam <cam@cam-X201EP.(none)>
2014-02-11 21:30:22 -0800

    basic api call

---
+++ api_test.py
@@ -0,0 +1,16 @@
+import requests
+import pprint
+from src.app.apikey import _API_KEY
+
+API_KEY = _API_KEY
+
+query_params = { 'apikey': API_KEY,
+                 'phrase': 'fiscal cliff' 
+               }
+
+endpoint = 'http://capitolwords.org/api/text.json'
+
+response = requests.get( endpoint, params=query_params)
+data = response.json
+
+pprint.pprint(data)
