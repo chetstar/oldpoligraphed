b13909a36894805c62f1fc03fd3c02085404cd25
Jordan <jordandietch@gmail.com>
2014-02-16 17:05:50 -0600

    move test foler to src folder

---
+++ src/test/app_test.py
@@ -0,0 +1,19 @@
+import unittest
+import PyClass_API_Project.src.app.model
+from PyClass_API_Project.src.app.apikey import _API_KEY
+from PyClass_API_Project.src.app.config import API_KEY
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_apikey(self):
+        expected = API_KEY
+        actual = _API_KEY
+
+        self.assertEquals(expected, actual, "apikey don't match: %s | %s" 
+            % (expected, actual))
