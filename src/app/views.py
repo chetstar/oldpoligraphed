2bd186425efe142a6b002388f4d5521513387831
Cam <cameron.bec@gmail.com>
2014-02-18 21:13:17 -0800

    Renamed the basejd.html and adjusted views.py

--- src/app/views.py
+++ src/app/views.py
@@ -15,7 +15,7 @@ def add_todo():
 @app.route('/')
 def index():
     todo_list = TodoItem.query.all()
-    return render_template('basejd.html', todos=todo_list)
+    return render_template('base.html', todos=todo_list)
 
 @app.route('/about')
 def about():
