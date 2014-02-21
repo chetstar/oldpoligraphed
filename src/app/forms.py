c11031ba4997960302c7c99a0f95519b49afb4c3
Jordan <jordandietch@gmail.com>
2014-02-21 13:09:42 -0800

    Initial Sign In Style Changes
    Here is the initial style changes to the sign in form. Used the
    bootstrap/wtf.html and quick_form. Will add some openid logos and
    center the form next.

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,7 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
+from wtforms import TextField, BooleanField, SubmitField
 from wtforms.validators import Required
 
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
+    submit = SubmitField('Submit')
