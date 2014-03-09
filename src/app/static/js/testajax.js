0faaab84114c202862426f3792a5c5301970873d
Jordan <jordandietch@gmail.com>
2014-03-09 16:06:36 -0700

    Functioning graph!
    We now have a functioning flot graph. Need to figure out how to
    customize this punk!

--- src/app/static/js/testajax.js
+++ src/app/static/js/testajax.js
@@ -1,5 +1,6 @@
   $(function() {
     var submit_form = function(e) {
+      var date_array = [];
       $.getJSON($SCRIPT_ROOT + '/_search_api', {
         a: $('input[name="a"]').val(),
         // b: $('input[name="b"]').val()
@@ -10,8 +11,14 @@
             data.results[i].count +
             " times by politicians on " +
             data.results[i].day +"</li>");
+          date_array.push([data.results[i].day,data.results[i].count]);
         }
         $("#keyword").text("The keyword " + $('input[name="a"]').val());
+        $.plot($("#placeholder"), [date_array], {xaxis: {
+    mode: "time",
+    timeformat: "%d"
+}}
+          );
         $('#result').text(data.user);
         $('input[name=a]').focus().select();
       });
