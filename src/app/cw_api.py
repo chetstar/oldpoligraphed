02d5ad8715548fcb54d3c8cc9e81f5a99afcc752
Jordan <jordandietch@gmail.com>
2014-03-16 14:06:40 -0700

    Add all days function
    Add all days in a date range to the results from the API response so
    that days with no count result can be plotted on the graph x-axis at 0.

--- src/app/cw_api.py
+++ src/app/cw_api.py
@@ -2,6 +2,7 @@ from apikey import _API_KEY
 import requests
 import json
 from date_convert import javascript_timestamp
+import datetime
 
 def cw_search_keywords(keywords):
     API_KEY = _API_KEY
@@ -10,7 +11,7 @@ def cw_search_keywords(keywords):
         query_params = {'apikey': API_KEY,
                     'phrase': keyword,
                     'start_date': '2014-01-01',
-                    'end_date': '2014-01-31',
+                    'end_date': '2014-02-28',
                     'granularity': 'day'
                     }
 
@@ -19,8 +20,35 @@ def cw_search_keywords(keywords):
         response = requests.get(endpoint, params=query_params)
         if response.status_code == 200:
             results = json.loads(response.text)
-            for result in results['results']:
+            results_entire_range = add_all_days('2014-01-01', '2014-02-28', results)
+            for result in results_entire_range['results']:
                 result['day'] = javascript_timestamp(result['day'])
             api_results.append(results)
 
     return api_results
+
+def add_all_days(start_date, end_date, result):
+
+    '''Function is used to add days with no results back into the list of results
+       so that the graph will plot a point of 0 for that day.'''
+
+    date_low = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    date_high = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+
+    returned_dates = []
+    for i in result['results']:
+       returned_dates.append(datetime.datetime.strptime(i['day'], '%Y-%m-%d'))
+
+    for i in range((date_high - date_low).days + 1):
+        date =  date_low + datetime.timedelta(i)
+        date_string = date.strftime('%Y-%m-%d')
+        if date not in returned_dates:
+            no_result = {"count": 0,
+                                "percentage": 0,
+                                "total": 0,
+                                "day": date_string,
+                                "raw_count": 0
+                                }
+            result['results'].insert(i, no_result)
+
+    return result
