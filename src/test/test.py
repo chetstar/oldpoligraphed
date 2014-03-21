34c16ab62c5cf6d96b677ce3fa7233c49b792190
chet star <cmeinzer@acbhcs.org>
2014-03-20 19:55:44 -0700

    test that work

--- src/test/test.py
+++ src/test/test.py
@@ -1,50 +1,57 @@
 #!flask/bin/python
-# import os
-# import unittest
-# import requests
-# import pprint
+import os
+import unittest
+import requests
+import pprint
+from apikey import _API_KEY
+from config import API_KEY
+from config import basedir
+from app import app, db
+from app.models import User
+import cw_api.py
 
-#from src.config import basedir
-# from app import app, db
-# from app.models import User
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['CSRF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+        self.app = app.test_client()
+        db.create_all()
 
-# from apikey import _API_KEY
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
-#class TestCase(unittest.TestCase):
-    # def setUp(self):
-    #     app.config['TESTING'] = True
-    #     app.config['CSRF_ENABLED'] = False
-    #     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
-    #     self.app = app.test_client()
-    #     db.create_all()
+    def test_make_unique_nickname(self):
+        u = User(nickname = 'john', email = 'john@example.com')
+        db.session.add(u)
+        db.session.commit()
+        nickname = User.make_unique_nickname('john')
+        assert nickname != 'john'
+        u = User(nickname = nickname, email = 'susan@example.com')
+        db.session.add(u)
+        db.session.commit()
+        nickname2 = User.make_unique_nickname('john')
+        assert nickname2 != 'john'
+        assert nickname2 != nickname
 
-    # def tearDown(self):
-    #     db.session.remove()
-    #     db.drop_all()
+    def test_API_KEY(self):
+        API_KEY = _API_KEY
+        query_params = { 'apikey': API_KEY,
+                         'phrase': 'fiscal cliff' 
+                       }
+        endpoint = 'http://capitolwords.org/api/text.json'
+        response = requests.get( endpoint, params=query_params)
+        data = response.json
+        pprint.pprint(data)
 
-    # def test_make_unique_nickname(self):
-    #     u = User(nickname = 'john', email = 'john@example.com')
-    #     db.session.add(u)
-    #     db.session.commit()
-    #     nickname = User.make_unique_nickname('john')
-    #     assert nickname != 'john'
-    #     u = User(nickname = nickname, email = 'susan@example.com')
-    #     db.session.add(u)
-    #     db.session.commit()
-    #     nickname2 = User.make_unique_nickname('john')
-    #     assert nickname2 != 'john'
-    #     assert nickname2 != nickname
+    def test_apikey(self):
+        expected = API_KEY
+        actual = _API_KEY
+
+        self.assertEquals(expected, actual, "apikey don't match: %s | %s"
+            % (expected, actual))
 
 
-    # def test_API_KEY(self):
-    #     API_KEY = _API_KEY
-    #     query_params = { 'apikey': API_KEY,
-    #                      'phrase': 'fiscal cliff' 
-    #                    }
-    #     endpoint = 'http://capitolwords.org/api/text.json'
-    #     response = requests.get( endpoint, params=query_params)
-    #     data = response.json
-    #     pprint.pprint(data)
-
-# if __name__ == '__main__':
-    # unittest.main()
+if __name__ == '__main__':
+    unittest.main()
