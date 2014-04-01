2711f18d4c38916aaac2cf769897d05d018ee94d
Jordan <jordandietch@gmail.com>
2014-04-01 00:15:11 -0700

    Merge branch 'master' of https://github.com/PyclassAPIProjectOrg/Poligraphed
    Conflicts:
    src/app/templates/base.html
    src/app/templates/testajax.html

--- src/app/views.py
+++ src/app/views.py
@@ -1,89 +1,10 @@
 from app import app, db, lm, oid
-from apikey import _API_KEY
-import requests
-import json
-from forms import LoginForm, SavedGraphForm, KeywordSearchForm, DeleteGraph, EditForm
+from forms import LoginForm, SavedGraphForm, DeleteGraph, EditForm
 from models import User, ROLE_USER, ROLE_ADMIN, SavedGraph
 from flask import render_template, flash, redirect, session, url_for, request, g, jsonify
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from email import send_email
 from cw_api import cw_search_keywords
-
-
-@app.route('/graph', methods=['GET', 'POST'])
-@login_required
-def graph():
-    user = g.user
-    form_keywords = None
-    saved_graphs = None
-    api_results = None
-    deleted_graph_id = None
-    deleted_graph = None
-    API_KEY = _API_KEY
-    keyword_form = KeywordSearchForm(prefix="keyword_form")
-    saved_graph_form = SavedGraphForm(prefix="saved_graph_form")
-    saved_graphs_query = SavedGraph.query.filter_by(user_id=user.id)
-    if saved_graphs_query:
-        saved_graphs = saved_graphs_query
-    delete_graph_form = DeleteGraph(prefix="delete_graph_form")
-
-    #If the keyword form is submitted
-
-    if keyword_form.validate_on_submit() and keyword_form.submit.data:
-
-        form_keywords = [
-            keyword_form.keyword_1.data,
-            keyword_form.keyword_2.data]
-
-        api_results = []
-        for keyword in form_keywords:
-
-            query_params = {'apikey': API_KEY,
-                            'phrase': keyword,
-                            'start_date': '2014-01-06',
-                            'end_date': '2014-01-11',
-                            'granularity': 'day'
-                            }
-
-            endpoint = 'http://capitolwords.org/api/dates.json'
-
-            response = requests.get(endpoint, params=query_params)
-            json_data = json.loads(response.text)
-
-            keyword_results = []
-            for item in json_data['results']:
-                keyword_results.append({item['day']: item['count']})
-
-            api_results.append((keyword, keyword_results))
-        return render_template('graph.html',
-                               saved_graphs=saved_graphs, graph=api_results,
-                               user=user, keyword_form=keyword_form,
-                               saved_graph_form=saved_graph_form,
-                               delete_graph_form=delete_graph_form)
-
-    if saved_graph_form.validate_on_submit() and saved_graph_form.submit.data:
-        save_graph = SavedGraph(graph_name=saved_graph_form.graph_name.data,
-                                keyword_1=saved_graph_form.keyword_1.data,
-                                keyword_2=saved_graph_form.keyword_2.data,
-                                user_id=user.id
-                                )
-        db.session.add(save_graph)
-        db.session.commit()
-        return redirect(url_for('graph'))
-
-    if delete_graph_form.validate_on_submit() and delete_graph_form.submit.data:
-        deleted_graph_id = delete_graph_form.graph_id.data
-        deleted_graph = db.session.query(SavedGraph).filter_by(id=deleted_graph_id).first()
-        db.session.delete(deleted_graph)
-        db.session.commit()
-        return redirect(url_for('graph'))
-
-    return render_template('graph.html',
-                           saved_graphs=saved_graphs, graph=api_results,
-                           user=user, keyword_form=keyword_form,
-                           saved_graph_form=saved_graph_form,
-                           delete_graph_form=delete_graph_form)
-    return redirect(url_for('test_save_graph'))
 
 
 @app.route('/')
@@ -197,7 +118,7 @@ def _search_api():
     return jsonify(keywords=api_results)
 
 
-@app.route('/testajax', methods=['GET', 'POST'])
+@app.route('/graph', methods=['GET', 'POST'])
 @login_required
 def test():
     user = g.user
@@ -229,7 +150,7 @@ def test():
         return redirect(url_for('test'))
 
     return render_template('testajax.html',
-                           saved_graphs = saved_graphs,
-                           user = user,
-                           saved_graph_form = saved_graph_form,
-                           delete_graph_form = delete_graph_form)
+                           saved_graphs=saved_graphs,
+                           user=user,
+                           saved_graph_form=saved_graph_form,
+                           delete_graph_form=delete_graph_form)
